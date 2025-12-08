e3aa0fd57314d899418cfaf92636eed3d336756b
Cécile Phan Nguyen <cecilephan@outlook.com>
2025-12-08 18:10:37 +0100

    feat: add fields

--- apps/frontend/src/pages/Register.jsx
+++ apps/frontend/src/pages/Register.jsx
@@ -5,19 +5,35 @@ export default function Register() {
     <>
       <section className="h-screen flex flex-col items-center justify-center gap-m">
         <form action="POST" className="flex flex-col items-center gap-sm">
-          <div className="flex flex-col">
+          <div className="flex flex-col w-full">
+            <label htmlFor="pseudo">pseudo</label>
+            <input type="text" name="pseudo" id="" />
+          </div>
+          <div className="flex gap-s">
+            <div className="flex flex-col">
+              <label htmlFor="pseudo">Prénom</label>
+              <input type="text" name="pseudo" id="" />
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="pseudo">Nom</label>
+              <input type="text" name="pseudo" id="" />
+            </div>
+          </div>
+          <div className="flex flex-col w-full">
             <label htmlFor="email">Adresse mail</label>
             <input type="email" name="email" id="" />
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col w-full">
             <label htmlFor="password">Mot de passe</label>
             <input type="password" name="password" id="" />
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col w-full">
             <label htmlFor="matchPassword">Confirmer mot de passe</label>
             <input type="password" name="matchPassword" id="" />
           </div>
-          <button type="submit">Créer un compte</button>
+          <button type="submit" className="w-full">
+            Créer un compte
+          </button>
         </form>
         <Link to="/login">Déjà un compte ?</Link>
       </section>
